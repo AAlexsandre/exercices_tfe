2c0844b3eb192ba912465b75502b8b8de6c11a3b
Alexsandre Akofovi <52098@etu.he2b.be>
2023-01-20 12:12:38 +0100

    sidebar : ok, modal : ok

--- 003_css_animation-journey/app.js
+++ 003_css_animation-journey/app.js
@@ -1,18 +1,31 @@
 const sidebarBtnToggle = document.getElementById("sidebarBtnToggle");
-// ajouter le script pour ajouter une class "open" pour la sidebar
-sidebarBtnToggle.addEventListener("click", changeClassForTheSideBar);
 
-function changeClassForTheSideBar() {
-    let openSideBar = document.getElementById("sidebar");
+// ajouter le script pour ajouter une class "open" pour la sidebar
+sidebarBtnToggle.addEventListener("click", function(){
     sidebarBtnToggle.classList.toggle("open");
-    openSideBar.classList.toggle("open");
-}
+    document.getElementById("sidebar").classList.toggle("open");
+});
 
 const modalBtn = document.getElementById("modalBtn");
-// ajouter le script pour ajouter une class "open" pour la modal
-modalBtn.addEventListener("click", changeClassForTheModal);
 
-function changeClassForTheModal() {
-    let openModal = document.getElementById("modal-element");
-    openModal.classList.toggle("open");
-}
+// ajouter le script pour ajouter une class "open" pour la modal
+modalBtn.addEventListener("click", function (){
+    document.getElementById("modal-element").classList.toggle("open");
+});
+
+
+let modal = document.getElementById("modal-element");
+
+document.getElementById("modalBtn").addEventListener("click",function(){
+    modal.style.display = "block";
+});
+
+document.getElementById("close-modal-btn").addEventListener("click",function(){
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
