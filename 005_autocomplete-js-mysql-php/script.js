bab765bd6a2814ba639d633e275a5fa39035a8cd
Akofovi Alexsandre <alexsandre.akofovi@gmail.com>
2023-02-10 19:44:13 +0100

    autocomplete finish

--- 005_autocomplete-js-mysql-php/script.js
+++ 005_autocomplete-js-mysql-php/script.js
@@ -1,12 +1,23 @@
-document.getElementById("auto-complete").addEventListener("input",function(){
-    fetch('getCities.php?name='+this.value)
-    .then((response) => response.json())
-    .then((data) => test(data));
+document.getElementById("auto-complete").addEventListener("input", function () {
+    fetch('getCities.php?name=' + this.value)
+        .then((response) => response.json())
+        .then((data) => displayOptions(data));
 });
 
 
-function test(data){
-    // for (let index = 0; index < data.length; index++) {
-    //     document.getElementById("results").appendChild(data[index]);
-    // }
+function displayOptions(data) {
+    let contentDiv = document.getElementById("results");
+    let p;
+
+    while (contentDiv.firstChild) {
+        contentDiv.removeChild(contentDiv.firstChild);
+    }
+    for (let i = 0; i < data.length; ++i) {
+        p = document.createElement("p");
+        p.classList = "m-1 ";
+        p.classList += "p-1 ";
+        p.classList += "bg-slate-200 ";
+        p.innerHTML = data[i];
+        contentDiv.appendChild(p);
+    }
 }
