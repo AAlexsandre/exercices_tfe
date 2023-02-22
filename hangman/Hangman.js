5febc1b3c4c20ba302af9587631c2fa0489a50d7
Akofovi Alexsandre <alexsandre.akofovi@gmail.com>
2023-02-22 12:10:31 +0100

    idea finish : need details

--- hangman/Hangman.js
+++ hangman/Hangman.js
@@ -17,23 +17,28 @@ export default class Hangman {
         this.render(container_id);
     }
 
-    createElement(game){
-        
+    createElement(game) {
+
         let clue = document.createElement("p");
         let findWord = document.createElement("p");
+        let placeLetter;
+        for (let i = 0; i < this.word.word.word.length; ++i) {
+            placeLetter = document.createElement("span");
+            placeLetter.id = i;
+            placeLetter.value = this.word.word.word.charAt(i);
+            placeLetter.innerHTML = "_ ";
+            findWord.appendChild(placeLetter);
+        }
         let lifes = document.createElement("p");
         let form = document.createElement("form");
         form.id = "inputForm";
         let enterLetter = document.createElement("input");
-        this.renderComponent(game,clue,findWord,lifes,form,enterLetter);
+        this.renderComponent(game, clue, findWord, lifes, form, enterLetter);
     }
 
-    renderComponent(game,clue,findWord,lifes,form,enterLetter){
+    renderComponent(game, clue, findWord, lifes, form, enterLetter) {
         clue.innerHTML = "Clue : " + this.word.word.clue;
-        for(let i = 0; i < this.word.sendTheLengthOfTheWorld(); ++i ){
-            findWord.innerHTML += "_ ";
-        }
-        if(this.word.sendTheLengthOfTheWorld() < 7){
+        if (this.word.sendTheLengthOfTheWorld() < 7) {
             this.chance = this.word.sendTheLengthOfTheWorld();
             lifes.innerHTML = "Remaining lives : " + this.word.sendTheLengthOfTheWorld();
         } else {
@@ -41,16 +46,45 @@ export default class Hangman {
         }
         enterLetter.placeholder = "Enter a letter";
         enterLetter.id = "try";
-        this.appenChild(game,clue,findWord,lifes,form,enterLetter);
+        this.appenChild(game, clue, findWord, lifes, form, enterLetter);
     }
 
-    appenChild(game,clue,findWord,lifes,form,enterLetter){
+    appenChild(game, clue, findWord, lifes, form, enterLetter) {
+        let found = false;
+        // let finish = false;
+        let sizeImage = ["150px", "225px", "300px", "375px", "450px", "525x"];
+        let index = 0;
         game.appendChild(clue);
         game.appendChild(findWord);
         game.appendChild(lifes);
         game.appendChild(form);
         inputForm.appendChild(enterLetter);
+        enterLetter.setAttribute("maxLength", 1);
+
+        enterLetter.addEventListener("keypress", (event) => {
+            if (event.key === "Enter" && this.chance != 0) {
+                event.preventDefault();
+                for (let i = 0; i < this.word.sendTheLengthOfTheWorld(); ++i) {
+                    if (enterLetter.value == this.word.word.word.charAt(i)) {
+                        found = true;
+                        game.children[2].children[i].innerHTML = enterLetter.value;
+                    }
+
+                    // if(game.children[2].children[i].innerHTML == "_ "){
+                    //     finish = false;
+                    // }
+                }
+
+                if (found == false) {
+                    this.chance--;
+                    lifes.innerHTML = "Remaining lives : " + this.chance;
+                    document.getElementById("image").style.width = sizeImage[index];
+                    index++;
+                }
+                enterLetter.value = "";
+                found = false;
+            }
+
+        });
     }
-
-
 }
